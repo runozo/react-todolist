7f6bc4303c1be1bf4887350a58f8374afdc6031c
Cesare Ghirelli <cghirelli@arpae.it>
2019-08-20 08:47:35 +0200

    add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -24,6 +24,25 @@ module.exports = {
         filteredTodos = filteredTodos.filter((todo) => {
             return !todo.completed || showCompleted;
         });
+
+        // Filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            return searchText.length === 0 || ~text.indexOf(searchText);
+        });
+
+
+
+        // Sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
         return filteredTodos;
 
     }
