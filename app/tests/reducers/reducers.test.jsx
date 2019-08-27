1675273e0f609c5364e9ca73a58b654600315c20
Cesare Ghirelli <cghirelli@arpae.it>
2019-08-27 11:03:05 +0200

    Add todos reducers and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -25,4 +25,52 @@ describe('Reducers', () => {
             expect(res).toEqual(true);
         });
     });
+
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'New todo'
+            }
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+
+        it('should correctly toggle todo status if not completed', () => {
+            var todos = [{
+                id: 1,
+                text: 'New todo',
+                completed: false,
+                createdAt: 789,
+                completedAt: undefined
+            }];
+
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: todos[0].id
+            }
+            var res = reducers.todosReducer(df(todos), df(action));
+            expect(res[0].completed).toEqual(!todos[0].completed);
+        });
+
+        it('should correctly toggle todo status if completed', () => {
+            var todos = [{
+                id: 1,
+                text: 'New todo',
+                completed: true,
+                createdAt: 456,
+                completedAt: 458
+            }];
+
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: todos[0].id
+            }
+            var res = reducers.todosReducer(df(todos), df(action));
+            expect(res[0].completed).toEqual(false);
+            expect(res[0].completedAt).toEqual(undefined);
+        });
+    });
 });
